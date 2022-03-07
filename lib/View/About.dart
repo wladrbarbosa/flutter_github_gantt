bc2e5322b980b63972dbf85a55acf45d74cdc1b8
Wladimir Roberto Barbosa <wladrbarbosa@gmail.com>
2022-03-06 22:28:57 -0300

    Alterada versão para v1.3.14

--- lib/View/About.dart
+++ lib/View/About.dart
@@ -37,7 +37,7 @@ class About extends StatelessWidget {
                           )
                         ),
                         TextSpan(
-                          text: 'Versão: v1.3.13',
+                          text: 'Versão: v1.3.14',
                           style: TextStyle(
                             color: Theme.of(context).primaryColor,
                             fontWeight: FontWeight.w500,
