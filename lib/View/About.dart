05fce96e1da3ae30383de456e2e5155feb789411
Wladimir Roberto Barbosa <wladrbarbosa@gmail.com>
2022-03-28 23:01:22 -0300

    Corrigido script de compilação CI/CD
    - Corrigida compilação para linux
    - Atualizada versão para v1.4.0+1

--- lib/View/About.dart
+++ lib/View/About.dart
@@ -37,7 +37,7 @@ class About extends StatelessWidget {
                           )
                         ),
                         TextSpan(
-                          text: 'Versão: v1.4.0',
+                          text: 'Versão: v1.4.0+1',
                           style: TextStyle(
                             color: Theme.of(context).primaryColor,
                             fontWeight: FontWeight.w500,
